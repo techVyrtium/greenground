8bfb4516b348ba77c457e71b5a9235e3cb5a6e43
fivantech <ing.ivanrojas@gmail.com>
2025-03-25 16:25:23 -0400

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        aa
         {children}
       </body>
     </html>
