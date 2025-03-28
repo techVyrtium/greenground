0f1753a8528c94584b5a3ea448dd9e05ef3a0b0c
ivantechdvv <irojas@vyrtium.com>
2025-03-27 22:18:18 -0400

    BannerAnimated

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import TopNavHero from "@/app/components/topNav";
 import WhatWeDo from "../components/whatWeDo";
+import ResponsibleCompany from "../components/responsibleCompany";
+import Experience from "../components/experience";
 export default function Home() {
   return (
-    <div className="w-full items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="w-full max-w-[1920px] mx-auto items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <TopNavHero />
       <WhatWeDo />
+      <ResponsibleCompany />
+      <Experience />
     </div>
   );
 }
