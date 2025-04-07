fca80e11b5a0957ffcb18a253c83bab823159189
ivantechdvv <irojas@vyrtium.com>
2025-04-07 12:02:24 -0400

    fix productcategories

--- src/app/components/productCategories.jsx
+++ src/app/components/productCategories.jsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import Link from "next/link";
 
 export const ProductCategories = () => {
   const title = "Conoce nuestras distintas categorías de productos:";
