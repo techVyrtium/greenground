c4d32efa0051da7a15c21ff037a24e3575feba9e
darius1751Vyrtium <lrivas@vyrtium.com>
2025-04-16 15:15:18 -0500

    feat: Implement map version mobile

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,6 +1,7 @@
 'use client'
 import Image from "next/image";
 import React, { Suspense, useEffect, useState } from "react";
+import { MapMobile } from "./MapMobile";
 const Spline = React.lazy(() => import("@splinetool/react-spline"));
 
 export default function MapW() {
@@ -17,7 +18,7 @@ export default function MapW() {
     <section className="w-full h-auto md:min-h-[400px] flex items-center justify-center my-8">
       <Suspense fallback={<div className="text-white">Cargando mapa...</div>}>
         <>
-          {screen >= 1024 ? (<Spline scene="https://prod.spline.design/WIoPFI60QecScwmI/scene.splinecode" />) : (<Image src={'/home/map.png'} width={444} height={665} className="my-20 w-full" />)}
+          {screen >= 1024 ? (<Spline scene="https://prod.spline.design/WIoPFI60QecScwmI/scene.splinecode" />) : (<MapMobile />)}
 
         </>
       </Suspense>
