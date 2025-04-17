c77a489d98037772cba0f6f225f9b96d011cdf4b
darius1751Vyrtium <lrivas@vyrtium.com>
2025-04-17 08:47:34 -0500

    refactor: Change system CardBlog

--- src/app/components/cardBlog.js
+++ src/app/components/cardBlog.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useParams } from "next/navigation";
@@ -8,13 +8,12 @@ export const Version = Object.freeze({
   TABLET: 2,
   DESK: 3
 });
-export default function CardBlog({ data, color, type, slide = 0, setSlide, version = Version.MOBILE }) {
-  const { locale } = useParams();
-  let $sliderMobile = useRef(null);
-  let $sliderDesk = useRef(null);
-  let $sliderTable = useRef(null);
 
-  const settings = {
+export default function CardBlog({ data, color, type, version = Version.MOBILE }) {
+  const { locale } = useParams();
+  let $slider = useRef(null);
+  const [slide, setSlide] = useState(0);
+  const desktop = {
     vertical: false,
     slidesToShow: 3,
     infinite: false,
@@ -38,159 +37,112 @@ export default function CardBlog({ data, color, type, slide = 0, setSlide, versi
     swipeToSlide: true,
     focusOnSelect: true,
   };
+  const settings = {
+    [Version.MOBILE]: mobile,
+    [Version.TABLET]: tablet,
+    [Version.DESK]: desktop
+  }
   useEffect(() => {
-    switch (version) {
-      case Version.MOBILE:
-        $sliderMobile.slickGoTo(slide);
-        break;
-      case Version.TABLET:
-        $sliderTable.slickGoTo(slide);
-        break;
-      default:
-        $sliderDesk.slickGoTo(slide);
+    $slider.slickGoTo(slide);
+  }, [slide]);
+
+  const handleNext = (cant) => {
+    if (slide === cant - 1) {
+      setSlide(0)
+      return;
     }
-  }, [slide])
+    setSlide(slide + 1);
+  };
+
+  const handlePrev = (cant) => {
+    if (slide === 0) {
+      setSlide(cant - 1)
+      return;
+    }
+    setSlide(slide - 1);
+
+  };
+
+  function Buttons({ slidesToShow, size }) {
+    return (
+      <div className="flex flex-row items-center justify-center gap-2">
+        <img
+          src={`${color ? "/home/arrowsOrangeL.png" : "/home/arrowsRSL.png"
+            }`}
+          alt="Descripción de la imagen"
+          className="mt-4 mr-2 cursor-pointer"
+          onClick={() => handlePrev(Math.ceil(size / slidesToShow))}
+        />
+        {new Array(Math.ceil(size / slidesToShow)).fill(0).map((_, i) => (
+          <div key={`dot-${i}`}
+            // onClick={() => setslide(i)}
+            className={`mt-4 p-3 rounded-full cursor-pointer ${slide == i && color
+              ? "bg-[#F19412]"
+              : slide == i && !color
+                ? "bg-[#B52C17]"
+                : "bg-[#FEF8F1]"
+              }`}
+            onClick={() => setSlide(i)}
+          ></div>
+        )
+        )}
+        <img
+          src={`${color ? "/home/arrowsOrangeR.png" : "/home/arrowsRSR.png"
+            }`}
+          alt="Descripción de la imagen"
+          className="mt-4 ml-2 cursor-pointer"
+          onClick={() => handleNext(Math.ceil(size / slidesToShow))}
+        />
+      </div>
+    );
+  }
   return (
-    <div className="w-full sm:max-h-[600px] overflow-hidden">
-      {
-        version === Version.MOBILE && (<div className="block md:hidden">
-          <Slider {...mobile} ref={sliderMobile => $sliderMobile = sliderMobile}>
-            {data.map((dato, i) => (
-              <div
-                key={i}
-                className="flex flex-col h-full bg-transparent items-stretch"
-              >
-                <div className="w-full h-auto object-cover bg-no-repeat bg-cover">
-                  <Image
-                    src={dato.image}
-                    className="w-full h-[300px] object-cover rounded-md"
-                    width={400}
-                    height={400}
-                  />
+    <div className="w-full overflow-hidden">
+      <div>
+        <Slider {...settings[version]} ref={slider => $slider = slider} afterChange={(currentSlide) => { setSlide(currentSlide) }}>
+          {data.map((dato, i) => (
+            <div
+              key={i}
+              className="flex flex-col h-full bg-transparent items-stretch px-2"
+            >
+              <div className="w-full h-auto object-cover bg-no-repeat bg-cover">
+                <Image
+                  src={dato.image}
+                  className="w-full h-[300px] object-cover rounded-md"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="pt-[8px] 2xl:pt-[16px] flex flex-col flex-grow">
+                <div
+                  className={`text-[28px] leading-8 h-16 overflow-y-scroll [scrollbar-width:none] ${color ? "text-[#D9840D]" : "text-[#B52C17]"
+                    } font-bold`}
+                >
+                  {dato.title}
                 </div>
-                <div className="pt-[8px] 2xl:pt-[16px] flex flex-col flex-grow">
-                  <div
-                    className={`text-[28px] leading-8 h-16 overflow-y-scroll [scrollbar-width:none] ${color ? "text-[#D9840D]" : "text-[#B52C17]"
-                      } font-bold`}
-                  >
-                    {dato.title}
+                <div className="block text-[#4a4a4a] font-[400] text-[clamp(1.375rem,4vw,1.75rem)] mt-[16px] leading-[1.1] flex-grow min-h-[123.188px]">
+                  <div className="line-clamp-4">
+                    {dato.description ?? dato.content[0].text}
                   </div>
-                  <div className="block text-[#4a4a4a] font-[400] text-[clamp(1.375rem,4vw,1.75rem)] mt-[16px] leading-[1.1] flex-grow min-h-[123.188px]">
-                    <div className="line-clamp-4">
-                      {dato.description ?? dato.content[0].text}
-                    </div>
-                  </div>
-                  <div className="flex w-full h-fit">
-                    <Link href={`/${type}/${dato.slug}`} className="w-full">
-                      <button
-                        className={`mt-[16px] w-full text-center text-[#EFEBE7] cursor-pointer ${color
-                          ? "bg-[#F19412] hover:bg-[#f19412e2]"
-                          : "bg-[#EA6B58] hover:bg-[#B52C17]"
-                          } font-[700] py-[4px] px-[16px] rounded-lg text-[clamp(1.375rem,4vw,1.75rem)] `}
-                      >
-                        {locale === "es" ? "Consulta" : "Find"}
-                      </button>
-                    </Link>
-                  </div>
+                </div>
+                <div className="flex w-full h-fit">
+                  <Link href={`/${type}/${dato.slug}`} className="w-full">
+                    <button
+                      className={`mt-[16px] w-full text-center text-[#EFEBE7] cursor-pointer ${color
+                        ? "bg-[#F19412] hover:bg-[#f19412e2]"
+                        : "bg-[#EA6B58] hover:bg-[#B52C17]"
+                        } font-[700] py-[4px] px-[16px] rounded-lg text-[clamp(1.375rem,4vw,1.75rem)] `}
+                    >
+                      {locale === "es" ? "Consulta" : "Find"}
+                    </button>
+                  </Link>
                 </div>
               </div>
-            ))}
-          </Slider >
-        </div >)
-      }
-
-      {
-        version === Version.TABLET && (<div className="hidden md:block lg:hidden">
-          <Slider {...tablet} ref={sliderTable => $sliderTable = sliderTable}>
-            {data.map((dato, i) => (
-              <div
-                key={i}
-                className="flex flex-col h-full bg-transparent items-stretch mx-2"
-              >
-                <div className="w-full h-auto object-cover bg-no-repeat bg-cover">
-                  <Image
-                    src={dato.image}
-                    className="w-full h-[300px] object-cover rounded-md"
-                    width={400}
-                    height={400}
-                  />
-                </div>
-                <div className="pt-[8px] 2xl:pt-[16px] flex flex-col flex-grow">
-                  <div
-                    className={`text-[28px] leading-8 h-16 overflow-y-scroll [scrollbar-width:none] ${color ? "text-[#D9840D]" : "text-[#B52C17]"
-                      } font-bold`}
-                  >
-                    {dato.title}
-                  </div>
-                  <div className="block text-[#4a4a4a] font-[400] text-[clamp(1.375rem,4vw,1.75rem)] mt-[16px] leading-[1.1] flex-grow min-h-[123.188px]">
-                    <div className="line-clamp-4">
-                      {dato.description ?? dato.content[0].text}
-                    </div>
-                  </div>
-                  <div className="flex w-full h-fit">
-                    <Link href={`/${type}/${dato.slug}`} className="w-full">
-                      <button
-                        className={`mt-[16px] w-full text-center text-[#EFEBE7] cursor-pointer ${color
-                          ? "bg-[#F19412] hover:bg-[#f19412e2]"
-                          : "bg-[#EA6B58] hover:bg-[#B52C17]"
-                          } font-[700] py-[4px] px-[16px] rounded-lg text-[clamp(1.375rem,4vw,1.75rem)] `}
-                      >
-                        {locale === "es" ? "Consulta" : "Find"}
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </Slider >
-        </div >)
-      }
-
-      {version == Version.DESK &&
-        <div className="hidden lg:block">
-          <Slider {...settings} ref={sliderDesk => $sliderDesk = sliderDesk}>
-            {data.map((dato, i) => (
-              <div
-                key={i}
-                className="flex flex-col h-full bg-transparent items-stretch px-3"
-              >
-                <div className="w-full h-auto object-cover bg-no-repeat bg-cover">
-                  <Image
-                    src={dato.image}
-                    className="w-full h-[300px] object-cover rounded-md"
-                    width={400}
-                    height={400}
-                  />
-                </div>
-                <div className="pt-[8px] 2xl:pt-[16px] flex flex-col flex-grow">
-                  <div
-                    className={`text-[28px] leading-8 h-16 overflow-y-scroll [scrollbar-width:none] ${color ? "text-[#D9840D]" : "text-[#B52C17]"
-                      } font-bold`}
-                  >
-                    {dato.title}
-                  </div>
-                  <div className="block text-[#4a4a4a] font-[400] text-[clamp(1.375rem,4vw,1.75rem)] mt-[16px] leading-[1.1] flex-grow min-h-[123.188px]">
-                    <div className="line-clamp-4">
-                      {dato.description ?? dato.content[0].text}
-                    </div>
-                  </div>
-                  <div className="flex w-full h-fit">
-                    <Link href={`/${type}/${dato.slug}`} className="w-full">
-                      <button
-                        className={`mt-[16px] w-full text-center text-[#EFEBE7] cursor-pointer ${color
-                          ? "bg-[#F19412] hover:bg-[#f19412e2]"
-                          : "bg-[#EA6B58] hover:bg-[#B52C17]"
-                          } font-[700] py-[4px] px-[16px] rounded-lg text-[clamp(1.375rem,4vw,1.75rem)] `}
-                      >
-                        {locale === "es" ? "Consulta" : "Find"}
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </Slider >
-        </div>}
+            </div>
+          ))}
+        </Slider >
+        <Buttons size={data.length} slidesToShow={settings[version].slidesToShow} />
+      </div >
     </div >
   );
 }
--- src/app/components/recetNews.js
+++ src/app/components/recetNews.js
@@ -12,11 +12,12 @@ export default function RecetNews({
   className = "",
 }) {
   const { locale } = useParams();
-  
+
   const [activeIndex, setActiveIndex] = useState(0);
-  const [dataDesk, setDataDesk] = useState([]);
-  const [dataTable, setDataTable] = useState([]);
-  const [dataMovil, setDataMovil] = useState([]);
+  const [data, setData] = useState([]);
+  // const [dataDesk, setDataDesk] = useState([]);
+  // const [dataTable, setDataTable] = useState([]);
+  // const [dataMovil, setDataMovil] = useState([]);
   const [colorset, setColorSet] = useState(true);
   const [isActive, setIsActive] = useState(true);
 
@@ -30,75 +31,6 @@ export default function RecetNews({
     }
   }, []);
 
-  const moveCard = (res) => {
-    if (activeIndex == 0) {
-      const rs = res.filter((r) => r.id == res[0].id || r.id == res[1].id);
-      setDataTable(rs);
-      const dm = res.filter((r) => r.id == res[0].id);
-      setDataMovil(dm);
-    }
-    if (activeIndex == 1) {
-      const rs = res.filter((r) => r.id == res[1].id || r.id == res[2].id);
-      setDataTable(rs);
-      const dm = res.filter((r) => r.id == res[1].id);
-      setDataMovil(dm);
-    }
-    if (activeIndex == 2) {
-      const dm = res.filter((r) => r.id == res[2].id);
-      setDataMovil(dm);
-    }
-  };
-
-  const handleNext = (cant) => {
-    if (activeIndex === cant - 1) {
-      setActiveIndex(0)
-      return;
-    }
-    setActiveIndex(activeIndex + 1);
-  };
-
-  const handlePrev = (cant) => {
-    if (activeIndex === 0) {
-      setActiveIndex(cant - 1)
-      return;
-    }
-    setActiveIndex(activeIndex - 1);
-
-  };
-
-  function Buttons({ slidesToShow, size }) {
-    return (
-      <div className="flex flex-row items-center justify-center gap-2">
-        <img
-          src={`${colorset ? "/home/arrowsOrangeL.png" : "/home/arrowsRSL.png"
-            }`}
-          alt="Descripción de la imagen"
-          className="mt-4 mr-2 cursor-pointer"
-          onClick={() => handlePrev(Math.ceil(size / slidesToShow))}
-        />
-        {new Array(Math.ceil(size / slidesToShow)).fill(0).map((_, i) => (
-          <div key={`dot-${i}`}
-            // onClick={() => setActiveIndex(i)}
-            className={`mt-4 p-3 rounded-full cursor-pointer ${activeIndex == i && colorset
-              ? "bg-[#F19412]"
-              : activeIndex == i && !colorset
-                ? "bg-[#B52C17]"
-                : "bg-[#FEF8F1]"
-              }`}
-          ></div>
-        )
-        )}
-        <img
-          src={`${colorset ? "/home/arrowsOrangeR.png" : "/home/arrowsRSR.png"
-            }`}
-          alt="Descripción de la imagen"
-          className="mt-4 ml-2 cursor-pointer"
-          onClick={() => handleNext(Math.ceil(size / slidesToShow))}
-        />
-      </div>
-    );
-  }
-
   const revData = async (rev) => {
     let res = null;
     if (rev == true) {
@@ -106,8 +38,7 @@ export default function RecetNews({
     } else if (rev == false) {
       res = news[locale];
     }
-    setDataDesk(res);
-    moveCard(res);
+    setData(res);
   };
 
   const changeColor = (c) => {
@@ -166,38 +97,29 @@ export default function RecetNews({
           >
             <div className="hidden lg:block">
               <CardBlog
-                data={dataDesk}
+                data={data}
                 color={colorset}
                 type={tipo === "recetas" ? "recipes" : "news"}
-                slide={activeIndex}
-                setSlide={setActiveIndex}
                 version={Version.DESK}
               />
-              <Buttons size={dataDesk.length} slidesToShow={3} />
             </div>
             {/*  Movil */}
             <div className="block lg:hidden sm:hidden">
               <CardBlog
-                data={dataMovil}
+                data={data}
                 color={colorset}
                 type={tipo === "recetas" ? "recipes" : "news"}
-                slide={activeIndex}
-                setSlide={setActiveIndex}
                 version={Version.MOBILE}
               />
-              <Buttons size={dataMovil.length} slidesToShow={1} />
             </div>
             {/*  Tabla */}
-            <div className="hidden md:block lg:hidden ">
+            <div className="hidden md:block lg:hidden">
               <CardBlog
-                data={dataTable}
+                data={data}
                 color={colorset}
                 type={tipo === "recetas" ? "recipes" : "news"}
-                slide={activeIndex}
-                setSlide={setActiveIndex}
                 version={Version.TABLET}
               />
-              <Buttons size={dataTable.length} slidesToShow={2} />
             </div>
           </motion.div>
         )}
